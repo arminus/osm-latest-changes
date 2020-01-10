622f2c2ecad5e8f80f81639f8dad2d37704dd14a
Rene <36115705+rene78@users.noreply.github.com>
2020-01-10 16:08:53 +0100

    Green color changed to a darker shade

--- js/site.js
+++ js/site.js
@@ -179,7 +179,7 @@ function run() {
                 var id = d.id ? d.id : d.feature.feature.properties.meta.changeset;
                 layer.eachLayer(function(l) {
                     if (l.feature.properties.meta.changeset == id) {
-                        l.setStyle({ color: '#0f0' });
+                        l.setStyle({ color: '#00c500' });
                     }
                 });
             }
